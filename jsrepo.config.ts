6b08a66099c2c72df4218e0b97e2fa6e823f82f6
Edward <11798919+Winter@users.noreply.github.com>
2026-02-12 16:50:33 +1100

    chore: initialize shadcn-svelte-extras

---
+++ jsrepo.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'jsrepo';
+
+export default defineConfig({
+    registries: ['@ieedan/shadcn-svelte-extras'],
+    paths: {
+		ui: '$lib/components/ui',
+		block: '$lib/components',
+		hook: '$lib/hooks',
+		action: '$lib/actions',
+		util: '$lib/utils',
+		lib: '$lib'
+	},
+});
